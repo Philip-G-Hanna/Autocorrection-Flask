843e70cb4ce959bce1066bd3953807014d89ca0b
Philip-G-Hanna <philip1805061@miuegypt.edu.eg>
2022-05-03 21:54:51 +0200

    Add

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+from models.user import User 
+
+user = User()
+
+app = Flask(__name__)
+
+msg = ''
+
+@app.route("/signin", methods=['POST', 'GET'])
+def signin():
+
+    global msg
+    if request.method == "POST":
+
+        account = user.login(request.form['email'], request.form['password'])
+
+        if(account and user.getType() == 1): #student
+            return render_template("index.html", utype=1)
+        elif(account and user.getType() == 2): #instructor
+            return render_template("index.html", utype=2)
+        else:
+            msg = "Incorrect email/password"
+            
+    return render_template("signin.html", msg=msg)
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+    # global msg
+    if request.method == "POST":
+        user.addUser(request.form['fname'], request.form['lname'], request.form['email'], request.form['phone'], request.form['password'], request.form['age'])
+        #!VALIDATION??#
+        return redirect(url_for("homepage"))
+    return render_template("signup.html")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/instructorprofile")
+def instructorprofile():
+    return render_template("instructorprofile.html", fn=user.getFname(),ln=user.getLname(),email=user.getEmail(),pn=user.getPhoneNumber())
+
+@app.route("/questionbank")
+def questionbank():
+    return render_template("questionbank.html")
+    
+if __name__ == "__main__":
+    app.run()
---
+++ models/user.py
@@ -0,0 +1,72 @@
+from models.dbConnection import DatabaseConnection
+
+class User:
+    db_connection = DatabaseConnection()
+    __conn = db_connection.get_conn()
+    __cursor = db_connection.get_cursor()
+
+    __id = None
+    __fname = None
+    __lname = None
+    __email = None
+    __phoneNumber = None
+    __password = None
+    __dob = None
+    __type = 1
+
+    def getID(self):
+        return self.__id
+
+    def getFname(self):
+       return self.__fname
+
+    def getLname(self):
+        return self.__lname
+
+    def getEmail(self):
+        return self.__email
+
+    def getPhoneNumber(self):
+        return self.__phoneNumber
+
+    def getPassword(self):
+        return self.__password
+
+    def getDob(self):
+        return self.__dob
+
+    def getType(self):
+        return self.__type
+
+    def addUser(self, fname, lname, email, pn, password, dob):
+        self.__fname = fname
+        self.__lname = lname
+        self.__email = email
+        self.__phoneNumber = pn
+        self.__password = password
+        self.__dob = dob
+
+        sql = "INSERT INTO `user`( `fname`, `lname`, `email`, `password`, `Type-id`, `age`, `phonenumber`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        val = (self.__fname, self.__lname, self.__email, self.__password, self.__type, self.__dob, self.__phoneNumber)    
+        self.__cursor.execute(sql, val)
+        self.__conn.commit()  
+        print("addUser")
+
+    def login(self, email, password):
+        self.__email = email
+        self.__password = password
+
+        sql = """SELECT * FROM user WHERE email = %s AND password = %s"""
+        self.__cursor.execute(sql, (self.__email, self.__password,))
+        result = self.__cursor.fetchone()
+        self.__conn.commit()
+        print("login", result)
+
+        if(result != None):
+            self.__id = result[0]
+            self.__fname = result[1]
+            self.__lname = result[2]
+            self.__type  = result[5]
+            self.__dob = result[7]
+            self.__phoneNumber = result[9]
+        return result
